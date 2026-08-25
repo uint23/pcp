5944a458a4c5716b73e8ea660698f140cedc3be4
Jonty Beglin <jontybeglin@gmail.com>
2026-08-25 18:33:25 +0100

    Remove pcpcc macros from utils.h

--- pcpcc/include/utils.h
+++ pcpcc/include/utils.h
@@ -1,14 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#ifndef PCPCC_VERSION
-#define PCPCC_VERSION "Unsure.."
-#endif /* PCPCC_VERSION */
-
-#define PCPCC_USAGE \
-	"\t[-v|--version]: Show pcpcc version" \
-	"\n"
-
 typedef enum {
 	ERR_OK,
 
@@ -21,3 +13,4 @@ typedef enum {
 void die(ErrorCode ec, const char* fmt, ...);
 
 #endif /* UTILS_H */
+
