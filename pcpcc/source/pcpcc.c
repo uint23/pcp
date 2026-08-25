b59940192fd8ff936f612d0e2555f467536cc748
uint <unsigned@netcat.uk>
2026-08-25 04:46:02 +0100

    Don't always die on parse_args

--- pcpcc/source/pcpcc.c
+++ pcpcc/source/pcpcc.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "utils.h"
@@ -12,10 +10,8 @@ static void parse_args(int argc, char* argv[])
 		if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0 )
 			die(ERR_OK, "%s ver. %s", argv[0], PCPCC_VERSION);
 
-		printf("%s: usage\n%s", argv[0], PCPCC_USAGE);
+		die(ERR_OK, "%s: usage\n%s", argv[0], PCPCC_USAGE);
 	}
-
-	exit(ERR_OK);
 }
 
 int main(int argc, char* argv[])
