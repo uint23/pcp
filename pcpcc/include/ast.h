4f19f4801511824079873826a13d08df0e71cbb5
uint <unsigned@netcat.uk>
2026-08-29 15:25:18 +0100

    cc/ast: Add AST representaiton skeleton

---
+++ pcpcc/include/ast.h
@@ -0,0 +1,53 @@
+#ifndef AST_H
+#define AST_H
+
+#include "lexer.h"
+
+typedef enum {
+	AST_ROOT,
+	AST_FUNCTION,
+	AST_BLOCK,
+	AST_RETURN,
+	AST_IDENTIFIER,
+	AST_LITERAL,
+	AST_BINARY,
+
+	AST_LAST
+} ASTType;
+
+typedef struct AST AST;
+struct AST {
+	ASTType type;
+
+	union {
+		Token token;
+
+		struct {
+			TokenType op;
+			AST*      left;
+			AST*      right;
+		} binary;
+
+		struct {
+			AST* expr;
+		} return_st;
+
+		struct {
+			AST**  items;
+			size_t count;
+		} block;
+
+		struct {
+			AST**  items;
+			size_t count;
+		}root;
+
+		struct {
+			Token name;
+			AST*  body;
+		} function;
+	} data;
+};
+
+#endif /* AST_H */
+
