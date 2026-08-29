f56e88e2c7bfe412657d8f50e9c7c7ec1d4d6daa
uint <unsigned@netcat.uk>
2026-08-29 17:46:20 +0100

    cc/parser: Initialise parser state, add parser helpers

--- pcpcc/include/parser.h
+++ pcpcc/include/parser.h
@@ -1,7 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-/* TODO */
+#include "ast.h"
+#include "common.h"
+
+/* initialise parser struct */
+void parser_init(SourceFile* source);
+
+/* parse through a file and return
+   complete AST */
+AST* parser_parse(void);
 
 #endif /* PARSER_H */
 
--- pcpcc/source/parser.c
+++ pcpcc/source/parser.c
@@ -1,2 +1,39 @@
+#include "ast.h"
+#include "lexer.h"
 #include "parser.h"
+
+static void advance(void);
+static int match(TokenType type);
+
+static struct {
+	Token cur;
+	Token prv;
+} parser;
+
+static void advance(void)
+{
+	parser.prv = parser.cur;
+	parser.cur = lexer_next();
+}
+
+static int match(TokenType type)
+{
+	if (type != parser.cur.type)
+		return 0;
+
+	advance();
+	return 1;
+}
+
+void parser_init(SourceFile* source)
+{
+	lexer_init(source);
+	parser.cur = lexer_next();
+}
+
+AST* parser_parse(void)
+{
+	AST* root = ast_new(AST_ROOT);
+	return root;
+}
 
